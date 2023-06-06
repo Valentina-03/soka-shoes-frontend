d6b8399f77a1ab4e1fc905fc4663d03b64dfbc7f
CamiloRamirezP <camiloandresrper@ufps.edu.co>
2023-06-06 16:09:00 -0500

    Empleado

--- src/app/administracion/page/empleado/empleado.component.ts
+++ src/app/administracion/page/empleado/empleado.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UsuarioService } from 'src/app/services/usuario.service';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
 })
 export class EmpleadoComponent implements OnInit {
- 
-  constructor() { }
+  usuarios:any [] = [];
 
+  constructor( private usuarioService:UsuarioService,
+  private router: Router,
+  private toastS: ToastrService) { }
+ 
   ngOnInit(): void {
+    this.usuarioService.getUsuarios().subscribe(usuarios=> this.usuarios = usuarios);
+
+  }
+  
+  editar(id:any){
+    this.router.navigateByUrl("/editarUsuario/"+id);
+  }
+
+  agregar(){
+    this.router.navigateByUrl("/nuevoempleado");
   }
 
 }
