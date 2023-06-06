04d1b45155116e02bab6145e32d076135e16cf1a
DanielaSanchezb <84026755+DanielaSanchezb@users.noreply.github.com>
2023-06-06 16:54:56 -0500

    Menu dashboard check

--- src/app/home/menu-admin/menu-admin.component.ts
+++ src/app/home/menu-admin/menu-admin.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-admin',
   templateUrl: './menu-admin.component.html',
+  styleUrls: ['./menu-admin.component.css']
 })
 export class MenuAdminComponent implements OnInit {
 
@@ -10,5 +11,6 @@ export class MenuAdminComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
 
 }
