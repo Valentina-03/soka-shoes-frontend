02453759838828625ff2f9ede22efd05b5275870
Jhonny Esneider Guarin Chavez <jhonnyesneidergcha@ufps.edu.co>
2023-06-06 18:33:09 -0500

    inicio img estatica

--- src/app/home/inicio/inicio.component.ts
+++ src/app/home/inicio/inicio.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CategoriaComponent } from 'src/app/administracion/page/categoria/categoria.component';
+import { CategoriaService } from 'src/app/services/categoria.service';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
+  imgs : any[] = [];
+  categorias : any[] = [];
+  constructor(private categoriaService : CategoriaService, private router : Router,private toastS: ToastrService) { }
 
   ngOnInit(): void {
+    this.categoriaService.getCategorias().subscribe(categorias => {
+      this.categorias = categorias
+      this.cargarImg();
+    } )
+    
   }
-
+  public cargarImg(){
+    for(let i = 0; i<this.categorias.length; i++)
+      this.categoriaService.getImgProduct(this.categorias[i].idCategoria).subscribe(img =>{
+        this.imgs[i] = URL.createObjectURL(img);
+        console.log(img);
+        
+      } );
+      
+  }
 }
