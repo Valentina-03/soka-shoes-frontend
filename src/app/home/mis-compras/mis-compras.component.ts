c84976e62b0ed702eb5cb5e0ba9da2c9b8f58f05
CamiloRamirezP <camiloandresrper@ufps.edu.co>
2023-06-06 20:04:15 -0500

    Mis compras

--- src/app/home/mis-compras/mis-compras.component.ts
+++ src/app/home/mis-compras/mis-compras.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CompraService } from 'src/app/services/compra.service';
 
 @Component({
   selector: 'app-mis-compras',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-compras.component.css']
 })
 export class MisComprasComponent implements OnInit {
+  compras:any [] = [];
 
-  constructor() { }
+  constructor(
+    private compraService:CompraService,
+    private router: Router,
+    private toastS: ToastrService,
+
+  ) { }
 
   ngOnInit(): void {
+    this.compraService.getCompras().subscribe(usuarios=> this.compras = usuarios);
+
   }
 
 }
